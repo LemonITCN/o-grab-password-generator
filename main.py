af633470c42d54cc5d53cdda7a17491cff9bab44
LemonITCN <liuri@lemonit.cn>
2020-03-23 15:43:30 +0800

    修改执行后提示符的形式输入密码

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import hmac
 import json
-import sys
 
 PASSWORD_KEY = '33854053'
 PASSWORD_FILE = 'password.lem'
@@ -15,4 +14,5 @@ def generate_password_secret(password):
         json.dump({PASSWORD_FIELD: signature}, dump_f)
 
 
-generate_password_secret(sys.argv[1])
+generate_password_secret(input('请输入密码（输入后按回车键生成密钥文件）:'))
+print('密码文件生成成功：', PASSWORD_FILE)
